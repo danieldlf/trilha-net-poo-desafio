6c983441d82a848deb27a74ffc68c123b08ebfbe
danieldlf <danieldlopesf@gmail.com>
2024-01-02 02:18:33 -0300

    Concluindo o desafio!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia1 = new Nokia("919291919", "Nokia 1", "123132113", 900000);
+Iphone iphone1 = new Iphone("9129312931292", "Iphone 1", "123231123", 90000000);
+
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("DIO");
+
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("DIO");
